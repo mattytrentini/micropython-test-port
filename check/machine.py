84cda6f5ec75a49e521ccdf6297cac4dcd9dcf4b
Ibrahim Abdelkader <i.abdalkader@gmail.com>
2022-11-01 04:06:51 +0200

    machine.py: Fix f-strings

--- check/machine.py
+++ check/machine.py
@@ -2,10 +2,10 @@ from machine import freq
 from config import alt_freq
 
 f = freq()
-print("Default frequency is {f}")
+print(f"Default frequency is {f}")
 if alt_freq:
     freq(alt_freq)  # Set alternate frequency
     if new_freq := freq() != alt_freq:
-        raise ValueError("Frequency didn't change as expected. freq()={new_freq}")
-    print("Alternate frequency set to {alt_freq}")
+        raise ValueError(f"Frequency didn't change as expected. freq()={new_freq}")
+    print(f"Alternate frequency set to {alt_freq}")
     freq(f) # Re-set default frequecy
